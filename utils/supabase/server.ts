843946d7c7bbd29538783aa70e68107107731dfe
arya <aryaatarzadeh@gmail.com>
2025-09-15 12:07:00 +0800

    Added Supabase client factory

---
+++ utils/supabase/server.ts
@@ -0,0 +1,33 @@
+// A server-side Supabase client factory That integrates cookie-based authentication 
+// So you can fetch Supabase data in Next.js server components, API routes, or middleware, 
+// while respecting the logged-in user’s session.
+
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function createClient() {
+  const cookieStore = await cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
