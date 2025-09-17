604eeb1e3bfc452087aacd8796d89b7ec41ef9ba
arya <aryaatarzadeh@gmail.com>
2025-09-18 00:36:20 +0800

    Implemented reset password functionality

--- app/(auth)/forgot-password/forgot-password-form.tsx
+++ app/(auth)/forgot-password/forgot-password-form.tsx
@@ -31,12 +31,11 @@ export function ForgotPasswordForm({
     setError(null);
 
     try {
-      // TODO:
-      // The url which will be included in the email. This URL needs to be configured in your redirect URLs in the Supabase dashboard at https://supabase.com/dashboard/project/_/auth/url-configuration
-      //   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      //     redirectTo: `${window.location.origin}/auth/update-password`,
-      //   });
-      //   if (error) throw error;
+      // The url which will be included in the email. This URL needs to be configured in the redirect URLs in the Supabase dashboard at https://supabase.com/dashboard/project/_/auth/url-configuration
+      const { error } = await supabase.auth.resetPasswordForEmail(email, {
+          redirectTo: `${window.location.origin}/update-password`,
+        });
+        if (error) throw error;
       setSuccess(true);
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "An error occurred");
