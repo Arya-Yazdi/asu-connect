d88a22439e98cedc843fba1c5b6b18f81dfb3659
arya <aryaatarzadeh@gmail.com>
2025-11-11 10:59:30 +0800

    updated Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/baguette.jpg"
-          alt="Bonjour"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">Baguette</li>
-          <li className="tracking-[-.01em]">Croissant</li>
-        </ol>
+        <h1 className="text-5xl text-gray-300">Coming Soon...</h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         {" "}
