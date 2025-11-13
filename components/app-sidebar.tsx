03c15c59a7afc064e271aad4107a4fa2ac52aa7b
arya <aryaatarzadeh@gmail.com>
2025-11-13 11:44:26 +0800

    developed and customized app specific sidebar component

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,87 @@
+import {
+  Calendar,
+  Home,
+  Inbox,
+  Link,
+  Search,
+  Settings,
+  UsersRound,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import { createClient } from "@/utils/supabase/server";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Clubs",
+    url: "clubs",
+    icon: UsersRound,
+  },
+  {
+    title: "Events",
+    url: "events",
+    icon: Calendar,
+  },
+];
+
+export async function AppSidebar() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const { data: profileData, error } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("user_id", user?.id)
+    .single();
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>ASU-Connect</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser
+          user={{
+            name: profileData?.name || "",
+            email: user?.email || "",
+            avatar: profileData?.avatar || "",
+          }}
+        />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
